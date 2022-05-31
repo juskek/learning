709756561ab5374d0a80131053498e035d7bbb06
Justin Kek <justinkekjq@gmail.com>
2022-05-31 12:48:06 +0100

    update

--- Programming/Python/packaging_tutorial/lib/lib.py
+++ Programming/Python/packaging_tutorial/lib/lib.py
@@ -1,3 +1,14 @@
+from pathlib import Path
+import pandas as pd
+import os
 def lib_function():
+    pathOfFile = Path(__file__).resolve()
+    pathOfData = os.path.join(pathOfFile.parent,'data')
     print('Hello from lib')
+    print(f'This file exists in {pathOfFile}')
+    df = pd.read_csv(os.path.join(pathOfData,'example.csv'))
+    print(df)
     
+    
+if __name__ == "__main__":
+    lib_function()
